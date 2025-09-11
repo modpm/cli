34ee78e21375424c9d5e7942d6a1f7c50fc8d36d
Zefir Kirilov <contact@zefir.pro>
2025-09-11 11:47:48 +0300

    update main command to use `cmd`

--- src/main.d
+++ src/main.d
@@ -2,15 +2,16 @@ import std.stdio;
 import core.stdc.stdlib : exit;
 
 import modpm.ver;
-import modpm.commander;
+import cmd;
 import modpm.commands;
 
 void main(string[] args) {
     new Program("modpm")
         .description("Modrinth package manager")
-        .ver(cliVersion, "-v, --version", "Show version information.")
-        .helpOption("-h, --help", "Show help for command.")
+        .versionString(cliVersion)
+        .versionOption("--version", "Show version information")
+        .helpOption("-h, --help", "Show help for command")
         .command(new InitCommand())
         .command(new HelpCommand())
-        .parse(args);
+        .run(args);
 }
