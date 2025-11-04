4973336992ab965245cff199188ade33b5277726
Zefir Kirilov <contact@zefir.pro>
2025-11-04 12:41:01 +0200

    install cmd 0.1.1

--- src/modpm/commands/init.d
+++ src/modpm/commands/init.d
@@ -2,15 +2,13 @@ module modpm.commands.init;
 
 import std.stdio;
 
-import modpm.commander;
+import cmd;
 
 public final class InitCommand : Command {
     this() {
         super("init")
             .description("Initialise a directory to manage.")
-            .flag("-t, --test", "Test flag")
-            .action((args, options, program) {
-                writeln("Init command!");
+            .action((args) {
                 return 0;
             });
     }
